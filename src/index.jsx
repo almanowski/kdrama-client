e5e43aa79b6ee77a68886b263e619f97146a1aa1
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-23 20:12:42 +0100

    Delete Navbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
-import {Container, Navbar, Nav} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 // Import statement to indicate that you need to budle ./index.scss
 import './index.scss';
@@ -10,33 +10,9 @@ import './index.scss';
 class kDramaApplication extends React.Component {
   render() {
     return (
-      <>
-        <Navbar variant="dark">
-            <Container className="nav-container">
-              <Navbar.Brand href="#home" className="ml-5">
-                <img
-                  alt="myDramas"
-                  src={"https://dl.dropboxusercontent.com/s/j8gnxagz3ia496k/logo.svg?dl=0"}
-                  crossOrigin ="anonymous"
-                  height="50"
-                  className="d-inline-block align-top"
-                />{' '}
-              </Navbar.Brand>
-              <Nav className="mr-auto">
-                <Nav.Link href="#">Dramas</Nav.Link>
-                <Nav.Link href="#">Genres</Nav.Link>
-                <Nav.Link href="#">Directors</Nav.Link>
-              </Nav>
-              <Nav className="mr-5">
-                <Nav.Link href="#">User</Nav.Link>
-              </Nav>
-            </Container>
-        </Navbar>
-
         <Container>
             <MainView />
         </Container>
-      </>
     );
   }
 }
