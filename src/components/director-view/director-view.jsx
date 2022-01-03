a001bb99a0d2b448b85a52545923e0d8d724c1f1
Anna Manowski <anna.l.manowski@gmail.com>
2022-01-03 22:21:07 +0100

    Add style and button

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,51 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {Container, Col, Row} from 'react-bootstrap';
 import {ArrowLeftCircle} from 'react-bootstrap-icons';
+
+import './director-view.scss';
 
 export function DirectorView(props) {
   const {director} = props;
   const {onBackClick} = props;
 
-    return (
-      <Container>
-        <div className="director-view">
+  return (
+    <Container>
+      <div className="director-view">
         <button className="back-button" onClick={() => { onBackClick(null) }}>
             <ArrowLeftCircle color="#07004d" size={50}/>  
-          </button>
+        </button>
         
-        <Row className="mt-5 mb-3">
+        <Row className="mt-1 director-view-row">
           <Col>
-            <Row className="mb-4">
-              <div className="director-name">
-                <h1>{director.Name}</h1>
-              </div>
+            <Row className="mb-4 director-name">
+              <h1>{director.Name}</h1>
             </Row>
-            <Row className="mb-4">
-            <div className="director-birth">
-                <p>{director.Birth}</p>
-              </div>
+            <Row className="mb-4 director-bio">
+              <p>{director.Bio}</p>
             </Row>
-            <Row className="mb-4">
-              <div className="director-bio">
-                <p>{director.Bio}</p>
-              </div>
+            <Row className="mb-4 director-birth">
+              <p>{director.Birth}</p>
             </Row>
           </Col>
         </Row>
 
-        </div>
-      </Container>
-    );
-};
-
-DirectorView.propTypes = {
-  drama: PropTypes.shape({
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Age: PropTypes.string.isRequired,
-    }).isRequired
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired
+      </div>
+    </Container>
+  );
 };
