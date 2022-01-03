8a72b32938f89c1e49d7e8476546a6efe4a65d33
Anna Manowski <anna.l.manowski@gmail.com>
2022-01-03 22:29:22 +0100

    Update genre-view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,43 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+/* import {useParams} from 'react-router-dom'; */
 import {Container, Col, Row} from 'react-bootstrap';
 import {ArrowLeftCircle} from 'react-bootstrap-icons';
 
 export function GenreView(props) {
-  const {genre} = props;
-  const {onBackClick} = props;
+  const {genre, onBackClick} = props;
+  /* const {genreName} = useParams(); */
 
-    return (
-      <Container>
-        <div className="genre-view">
+  return (
+    <Container>
+      <div className="genre-view">
         <button className="back-button" onClick={() => { onBackClick(null) }}>
             <ArrowLeftCircle color="#07004d" size={50}/>  
-          </button>
+        </button>
         
-        <Row className="mt-5 mb-3">
+        <Row className="mt-1 genre-view-row">
           <Col>
-            <Row className="mb-4">
-              <div className="genre-name">
-                <h1>{genre.Name}</h1>
-              </div>
+            <Row className="mb-4 genre-name">
+              <h1>{genre.Name}</h1>
             </Row>
-            <Row className="mb-4">
-            <div className="genre-birth">
-                <p>{genre.Description}</p>
-              </div>
+            <Row className="mb-4 genre-description">
+              <p>{genre.Description}</p>
             </Row>
           </Col>
         </Row>
 
-        </div>
-      </Container>
-    );
+    </div>
+  </Container>
+  )
 };
 
-GenreView.propTypes = {
-  genre: PropTypes.shape({
-    Name: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired
-};
