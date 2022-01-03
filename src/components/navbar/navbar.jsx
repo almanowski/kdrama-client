54ef01403b3eb42d76103df8554e603f2a66192f
Anna Manowski <anna.l.manowski@gmail.com>
2022-01-03 22:20:16 +0100

    Add condition

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {link} from 'react-router-dom';
-import {Container, Navbar, Nav, Button, NavDropdown} from 'react-bootstrap';
+import {Container, Navbar, Nav, Button, NavDropdown, Row} from 'react-bootstrap';
+
+import './navbar.scss'
 
 export function Navbar() {
   const user = localStorage.getItem('user');
@@ -10,21 +12,38 @@ export function Navbar() {
     window.open('/', '_self');
   }
 
+  if (user) {
+    return (
+      <Navbar className="main-nav" sticky="top" expand="lg" variant="dark">
+        <Container className="nav-container">
+          <Navbar.Brand href="/" className="navbar-brand">
+            <img className="d-inline-block align-top" alt="myDramas" src={"https://dl.dropboxusercontent.com/s/j8gnxagz3ia496k/logo.svg?dl=0"} crossOrigin ="anonymous" height="50" />{' '}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="/" className="ml-2">
+                Dramas
+              </Nav.Link>
+            </Nav>
+            <Nav className="ml-auto mr-4">
+              <NavDropdown title={user} id="basic-nav-dropdown">
+              <NavDropdown.Item href={`/users/${user}`}>Profil</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => {this.onLoggedOut() }}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
+
   return (
     <Navbar className="main-nav" sticky="top" expand="lg" variant="dark">
       <Container className="nav-container">
-      <Navbar.Brand href="/" className="ml-5">
+        <Navbar.Brand href="/" className="navbar-brand">
           <img className="d-inline-block align-top" alt="myDramas" src={"https://dl.dropboxusercontent.com/s/j8gnxagz3ia496k/logo.svg?dl=0"} crossOrigin ="anonymous" height="50" />{' '}
-      </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <NavDropdown title={user} id="basic-nav-dropdown">
-            <NavDropdown.Item href={`/users/${user}`}>Profil</NavDropdown.Item>
-            <NavDropdown.Item onClick={() => {this.onLoggedOut() }}>Logout</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
