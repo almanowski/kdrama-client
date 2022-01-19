a0342ddab470e36288ad3a6069f12ae0c257ab08
Anna Manowski <anna.l.manowski@gmail.com>
2022-01-19 12:04:04 +0100

    Create visibility-filter-input.jsx

---
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,13 @@
+import Reacht from 'react';
+import {connect} from 'react-redux';
+import Form from 'react-bootstrap/Form';
+
+import {setFilter} from '../../actions/actions';
+
+import './visibility-filter-input.scss'
+
+function VisibilityFilterInput(props) {
+    return <Form.Control onChange={e => props.setFilter(e.target.value)} value={props.VisibilityFilter} placeholder="Search drama..." className="drama-search" />;
+}
+
+export default connect(null, {setFilter})(VisibilityFilterInput);
